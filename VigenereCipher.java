049cc724a9060696c313e6db5c96e9639614eb5f
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-13 19:21:41 +0100

    Encrypt() implemented

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -14,7 +14,7 @@ public class VigenereCipher {
             int input = scan.nextInt();
 
             if (input == 1) {
-                System.out.println("You have entered Encrypt()");
+                Encrypt();
             } else if (input == 2) {
                 System.out.println("You have entered Decrypt()");
             } else if (input == 3) {
@@ -25,4 +25,43 @@ public class VigenereCipher {
             }
         } // end while
     }// end main()
+
+    public static void Encrypt()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You have entered Encrypt!");
+        System.out.println("Enter plaintext to be encrypted:");
+        String plaintext = scan.nextLine().toUpperCase();
+        System.out.println("Enter the the key string which will be used to encrypt:");
+        String key = scan.nextLine().toUpperCase();
+        String answer = "";
+        
+        for (int i = 0, j = 0; i < plaintext.length(); i++) 
+        {
+            //Getting the individual letter
+            char letter = plaintext.charAt(i);
+            if (letter >= 'A' && letter <= 'Z')
+            {
+                //We have the message which a letter will be assigned or added to
+                //(char) will let us parse the result into a char
+                //Bellow is the algorithm I used to achieve the encrypting
+                //First we do the multiplication of 2 * 'A' 65
+                //Then, we add the letter value with the key value and subrtract both
+                //We found the modulo of the value we just got and 26
+                //Add it to 'A' (value)
+                //This is our encrypted letter
+            	answer += (char)( (letter + key.charAt(j) - 2 * 'A') % 26 + 'A');
+                //This will allow us to iterate through the key elements one by one
+            	j = ++j % key.length();
+            }
+            else
+            {
+                //Else if the char is not a upper case letter just add it to the message
+            	answer = answer + letter;
+            }
+        }
+
+        System.out.println(plaintext + " encrypted using key of " + key + " = " + answer);
+
+    }// end encrypt()
 }// end VigenereCipher
