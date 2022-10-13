3bd7385b436dd79dfc6e8fd99119a0346468883c
Jonathan Hew <71874041+JonathanHew@users.noreply.github.com>
2022-10-13 19:34:27 +0100

    Decrypt() implemented

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -16,7 +16,7 @@ public class VigenereCipher {
             if (input == 1) {
                 Encrypt();
             } else if (input == 2) {
-                System.out.println("You have entered Decrypt()");
+                Decrypt();
             } else if (input == 3) {
                 System.out.println("Goodbye!");
                 break;
@@ -26,8 +26,7 @@ public class VigenereCipher {
         } // end while
     }// end main()
 
-    public static void Encrypt()
-    {
+    public static void Encrypt() {
         Scanner scan = new Scanner(System.in);
         System.out.println("You have entered Encrypt!");
         System.out.println("Enter plaintext to be encrypted:");
@@ -35,22 +34,54 @@ public class VigenereCipher {
         System.out.println("Enter the the key string which will be used to encrypt:");
         String key = scan.nextLine().toUpperCase();
         String answer = "";
-        
-        for (int i = 0, j = 0; i < plaintext.length(); i++) 
-        {
-            //Getting the individual letter
+
+        for (int i = 0, j = 0; i < plaintext.length(); i++) {
+            // capturing the letter in plain text string
             char letter = plaintext.charAt(i);
+            if (letter >= 'A' && letter <= 'Z') {
+                // (char) will let us parse the result into a char
+                // Bellow is the algorithm I used to achieve the encrypting
+                // First we do the multiplication of 2 * 'A' 65
+                // Then, we add the letter value with the key value and subtract both
+                // We found the modulo of the value we just got and 26
+                // Add it to 'A' (value)
+                // This is our encrypted letter
+                answer += (char) ((letter + key.charAt(j) - 2 * 'A') % 26 + 'A');
+                // This will allow us to iterate through the key elements one by one
+                j = ++j % key.length();
+            } else {
+                // Else if the char is not a upper case letter just add it to the message
+                answer = answer + letter;
+            }
+        }
+
+        System.out.println(plaintext + " encrypted using key of " + key + " = " + answer);
+
+    }// end encrypt()
+
+    public static void Decrypt() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You have entered Decrypt!");
+        System.out.println("Enter text to be decrypted:");
+        String text = scan.nextLine().toUpperCase();
+        System.out.println("Enter the the key string which will be used to decrypt:");
+        String key = scan.nextLine().toUpperCase();
+        String answer = "";
+
+        //For loop to iterate through encrypted text
+        for (int i = 0, j = 0; i < text.length(); i++) 
+        {
+            //capturing the letter in the text
+            char letter = text.charAt(i);
             if (letter >= 'A' && letter <= 'Z')
             {
-                //We have the message which a letter will be assigned or added to
                 //(char) will let us parse the result into a char
-                //Bellow is the algorithm I used to achieve the encrypting
-                //First we do the multiplication of 2 * 'A' 65
-                //Then, we add the letter value with the key value and subrtract both
-                //We found the modulo of the value we just got and 26
-                //Add it to 'A' (value)
-                //This is our encrypted letter
-            	answer += (char)( (letter + key.charAt(j) - 2 * 'A') % 26 + 'A');
+                //Bellow is the algorithm I used to achieve the decrypting
+                //Firstly, we subtract the two values of letter and key at j position
+                //We subtract it from 26 sum of the alphabet
+                //Perfom the modulo operation 26
+                //Add the result to the value of 'A'
+            	answer += (char)(((letter - key.charAt(j) + 26) % 26) + 'A');
                 //This will allow us to iterate through the key elements one by one
             	j = ++j % key.length();
             }
@@ -61,7 +92,6 @@ public class VigenereCipher {
             }
         }
 
-        System.out.println(plaintext + " encrypted using key of " + key + " = " + answer);
-
-    }// end encrypt()
+        System.out.println(text + " decrypted using key of " + key + " = \n\n" + answer);
+    }
 }// end VigenereCipher
